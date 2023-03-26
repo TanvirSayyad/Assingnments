91f385fdd515433d1691cc81bbf8d35691b979ba
TanvirSayyad <102937392+TanvirSayyad@users.noreply.github.com>
2023-03-27 01:38:51 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package edubridge;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!!!");
+		System.out.println("My name is tanvir");
+		System.out.println("this my first project");
+		
+		
+	}
+	
+
+}
